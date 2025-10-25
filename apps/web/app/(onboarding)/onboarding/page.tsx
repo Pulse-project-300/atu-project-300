de974c7656c68e9e1dbc3648e463c10af2b94031
tadhgdowdall <tadhg.jamesdowdall@gmail.com>
2025-10-25 17:40:14 +0100

    chore: removed unnecesary function

--- apps/web/app/(onboarding)/onboarding/page.tsx
+++ apps/web/app/(onboarding)/onboarding/page.tsx
@@ -1,32 +1,8 @@
 "use client";
 
 import { OnboardingContainer } from "@/components/onboarding/onboarding-container";
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
 
 export default function OnboardingPage() {
-  const [status, setStatus] = useState("");
-
-  // Currently not being used as it is being handled in onboarding-container
-  async function handleGenerate() {
-    setStatus("Generating...");
-    try {
-      const res = await fetch("/api/plans/generate", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          userId: "demo-user",
-          profile: { goal: "muscle gain", experience: "beginner" },
-        }),
-      });
-
-      const data = await res.json();
-      setStatus(JSON.stringify(data, null, 2));
-    } catch (err: any) {
-      setStatus(err.message);
-    }
-  }
-
   return (
     <div>
       <OnboardingContainer />
